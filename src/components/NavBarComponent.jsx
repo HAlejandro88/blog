09bd07184cc557893062f9d7b758458363b4129e
Héctor Alejandro Soto García <asoto@grupo-siayec.com.mx>
2023-05-12 18:40:04 -0600

    fix: add hover in li list

--- src/components/NavBarComponent.jsx
+++ src/components/NavBarComponent.jsx
@@ -7,10 +7,10 @@ const NavBarComponent = () => {
         <div className="container mx-auto px-4 flex items-center justify-evenly  bg-black">
             <LogoComponent/>
             <ul className="flex justify-center items-center gap-4  w-1/3">
-                <li className="">Home</li>
-                <li className="">About</li>
-                <li className="">Blog</li>
-                <li className="">Contact</li>
+                <li className="hover:cursor-pointer">Home</li>
+                <li className="hover:cursor-pointer">About</li>
+                <li className="hover:cursor-pointer">Blog</li>
+                <li className="hover:cursor-pointer">Contact</li>
             </ul>
             <SearchComponent/>
         </div>
